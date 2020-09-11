23a7e55477cee7262f24c1a61bfd130432807d5d
Kyle Barthelmes <kbart2401@gmail.com>
2020-09-11 17:12:59 -0400

    Indicate Playerz

--- connect-four.js
+++ connect-four.js
@@ -1,7 +1,10 @@
 import { Player } from './player_class.js';
 import { Column } from './column_class.js';
 import { Game } from './game-classes.js';
+
+////////////GLOBAL VARIABLES////////////////
 let game = undefined;
+////////////GLOBAL METHODS////////////////
 function updateUI(game){
     let board = document.getElementById("board-holder")
     if(game === undefined){
@@ -9,9 +12,22 @@ function updateUI(game){
     } else {
         board.classList.remove("is-invisible")
     }
-    document.getElementById("game-name").innerHTML = game.getName()
+    document.getElementById("game-name").innerHTML = game.getName();
+   game.playInColumn();
+    let clickTargets = document.getElementById('click-targets')
+    if(game.playInColumn() === 1) {
+        clickTargets.classList.add('red')
+        clickTargets.classList.remove('black')
+    }
+    else {
+        clickTargets.classList.add('black');
+        clickTargets.classList.remove('red');
+
+    }
+
 }
 
+///////////DOM Content Loaded//////////////
 window.addEventListener("DOMContentLoaded", () => {
     let player1 = document.getElementById("player-1-name")
     let player2 = document.getElementById("player-2-name")
@@ -36,57 +52,61 @@ newGameBtn.addEventListener("click", event =>{
 
 })
 
-
-
-
-
-
-const column0 = new Column("0");
-const column1 = new Column("1");
-const column2 = new Column("2");
-const column3 = new Column("3");
-const column4 = new Column("4");
-const column5 = new Column("5");
-const column6 = new Column("6");
-let instanceArr = [column0,column1, column2, column3, column4, column5, column6]
-
-let counter = 0;
 document.getElementById('click-targets').addEventListener('click', event => {
-
-
-    //get event target id, save to variable
-         //check last index of variable
-         //if column includes that index, push 0 to column
-         let tokenId = event.target.id;
-
-         counter++;
-
-         //need last index of the id and push into empty array
-
-    instanceArr.forEach(function(el){
-    if(el.name.includes(tokenId[tokenId.length - 1])){
-        el.addToken("red");
-        let squareDivs = Array.from(document.querySelectorAll(".token-square"))
-        for(let i = 0; i < squareDivs.length; i++){
-            let sqDivId = squareDivs[i].id;
-            let sqNum = sqDivId[sqDivId.length - 1];
-            console.log(sqNum)
-
-            // console.log(sqDivId)
-            if(el.name.includes(sqNum)){
-
-              let redToken = document.createElement("div");
-              redToken.classList.add("token","red");
-              let sqDiv = squareDivs[i]
-                sqDiv.appendChild(redToken)
-
-            }
-
-
-        }
-
-    }
-    })
-
+    game.playInColumn();
+    updateUI(game);
 })
+
+
+
+
+
+// const column0 = new Column("0");
+// const column1 = new Column("1");
+// const column2 = new Column("2");
+// const column3 = new Column("3");
+// const column4 = new Column("4");
+// const column5 = new Column("5");
+// const column6 = new Column("6");
+// let instanceArr = [column0,column1, column2, column3, column4, column5, column6]
+
+// let counter = 0;
+// document.getElementById('click-targets').addEventListener('click', event => {
+
+
+//     //get event target id, save to variable
+//          //check last index of variable
+//          //if column includes that index, push 0 to column
+//          let tokenId = event.target.id;
+
+//          counter++;
+
+//          //need last index of the id and push into empty array
+
+//     instanceArr.forEach(function(el){
+//     if(el.name.includes(tokenId[tokenId.length - 1])){
+//         el.addToken("red");
+//         let squareDivs = Array.from(document.querySelectorAll(".token-square"))
+//         for(let i = 0; i < squareDivs.length; i++){
+//             let sqDivId = squareDivs[i].id;
+//             let sqNum = sqDivId[sqDivId.length - 1];
+//             console.log(sqNum)
+
+//             // console.log(sqDivId)
+//             if(el.name.includes(sqNum)){
+
+//               let redToken = document.createElement("div");
+//               redToken.classList.add("token","red");
+//               let sqDiv = squareDivs[i]
+//                 sqDiv.appendChild(redToken)
+
+//             }
+
+
+//         }
+
+//     }
+//     })
+
+// })
 })
--- game-classes.js
+++ game-classes.js
@@ -1,10 +1,22 @@
 class Game{
-    constructor(player1Name, player2Name){
+    constructor(player1Name, player2Name, currentPlayer = 1){
         this.player1Name = player1Name;
-        this.player2Name = player2Name
+        this.player2Name = player2Name;
+        this.currentPlayer = currentPlayer;
     }
     getName(){
         return this.player1Name + " vs " + this.player2Name
     }
+    playInColumn() {
+        if(this.currentPlayer === 1) {
+        this.currentPlayer = 2;
+        }
+        else {
+            this.currentPlayer = 1;
+        }
+        return this.currentPlayer;
+    }
 }
+
+
 export { Game }
